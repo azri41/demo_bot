77065a0e4e0884f727e8fdeb166053a548fd19a9
Azri Azmi <azri041@gmail.com>
2020-12-15 09:57:37 +0800

    test

--- bot/app.py
+++ bot/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify,request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import os
-import train.py
+import train
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
